21f6e9e59f3505bb630b7b66af985b298bfde977
jona4815 <99785626+Raque48@users.noreply.github.com>
2023-04-14 00:20:14 -0600

    Update HomeController.java

--- src/main/java/me/parzibyte/sistemaventasspringboot/HomeController.java
+++ src/main/java/me/parzibyte/sistemaventasspringboot/HomeController.java
@@ -1,1 +1,20 @@
 
+package me.parzibyte.sistemaventasspringboot;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class HomeController {
+	
+	 
+	 @GetMapping("/")
+	public String mostrarIndex(Model model) {
+		return"productos/ver_productos";
+	}
+	 
